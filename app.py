4716ae4dc5c88a8dbba87da6a959c94ecac88d48
QuinlanVic <Quinlanvc@gmail.com>
2024-03-26 11:53:16 +0200

    New db connection & updates

--- app.py
+++ app.py
@@ -1,9 +1,132 @@
 from flask import Flask, jsonify, request, render_template
 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
+import os
+from dotenv import load_dotenv
+from pprint import pprint
 import json
 
+# All to keep away private passwords and stuff away from the public via
+# puts variables in .env file into windows environmental variables
+load_dotenv()  # load -> os env (environmental variables)
+# print(os.environ.get("AZURE_DATABASE_URL"))
 # create an instance of the 'Flask' class
 app = Flask(__name__)
+
+# General Pattern
+# mssql+pyodbc://<username>:<password>@<dsn_name>?driver=<driver_name>
+
+# change connection string when working with different databases
+connection_string = os.environ.get("AZURE_DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = connection_string
+db = SQLAlchemy(app)  # ORM
+
+# to test connection
+try:
+    with app.app_context():
+        # Use text() to explicitly declare your SQL command
+        result = db.session.execute(text("SELECT 1")).fetchall()
+        print("Connection successful:", result)
+except Exception as e:
+    print("Error connecting to the database:", e)
+
+
+# Model (SQLAlchemy) == Schema
+# CREATE TABLE movies (
+# 	has to be varchar so it can be indexed for increased performance (TEXT cannot be indexed)
+# 	id VARCHAR(50) PRIMARY KEY,
+# 	name VARCHAR(100),
+# 	poster VARCHAR(255),
+# 	rating FLOAT,
+# 	summary VARchAR(500),
+# 	trailer VARCHAR(255)
+# )
+
+
+# connect to our azure
+# constructor we are using is from "db.Model"
+class Movie(db.Model):
+    __tablename__ = "movies"
+    id = db.Column(db.String(50), primary_key=True)
+    name = db.Column(db.String(50))
+    poster = db.Column(db.String(50))
+    rating = db.Column(db.Float(50))
+    summary = db.Column(db.String(50))
+    trailer = db.Column(db.String(50))
+
+    # JSON - Keys (can change names sent to front-end)
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "poster": self.poster,
+            "rating": self.rating,
+            "summary": self.summary,
+            "trailer": self.trailer,
+        }
+
+
+# new get request getting data from azure
+@app.get("/movies")
+def get_movies():
+    movie_list = Movie.query.all()  # SELECT * FROM movies | movie_list iterator
+    data = [movie.to_dict() for movie in movie_list]  # convert to a list of dict
+    # print(data)
+    # print(type(jsonify(data)))
+    # return jsonify(data)
+    return data
+
+
+# Task 2 - get movies from database
+# movies list
+@app.route("/movieslist")
+def movie_list_page():
+    movie_list = Movie.query.all()  # SELECT * FROM movies | movie_list iterator
+    # print(type(movie_list)) # list
+    # print(type(movie_list[0])) # app.Movie
+    data = [movie.to_dict() for movie in movie_list]  # convert to a list of dict
+    return render_template("movies-list.html", movies=data)
+
+
+# Task 3 - display the data for specific movie on page
+# specific movie page
+@app.route("/movieslist/<id>")
+def movie_page(id):
+    specific_movie = Movie.query.get(id)
+
+    if specific_movie is None:
+        return "<h1>Movie not found</h1>"
+
+    return render_template("movie.html", movie=specific_movie.to_dict())
+
+
+# Task 1
+# .all() - .get()
+# <variable_name> -> becomes the keyword argument to "get_specific_movie"
+@app.get("/movies/<id>")
+def get_specific_movie(id):
+    # print(type(id))  # string
+    # movie_list = Movie.query.all()  # SELECT * FROM movies | movie_list iterator
+    # data = [movie.to_dict() for movie in movie_list]  # convert to a list of dict
+    # or - generator expression + have to account for when nothing is found (default val = None)
+    # specific_movie = next((movie for movie in data if movie["id"] == id), None)
+    # print(type(specific_movie))
+
+    # get specific movie
+    specific_movie2 = Movie.query.get(id)
+
+    # if specific_movie == []:
+    if specific_movie2 is None:
+        result = {"message": "movie not found"}
+        return jsonify(result), 404
+
+    # convert to a dictionary
+    data = specific_movie2.to_dict()
+    result = {"message": "movie successfully found", "data": data}
+    return jsonify(result)
+
 
 movies = [
     {
@@ -51,7 +174,7 @@ movies = [
         "name": "The Avengers",
         "rating": 8,
         "summary": "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
-        "poster": "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
+        "poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
         "trailer": "https://www.youtube.com/embed/eOrNdBpGMv8",
     },
     {
@@ -145,27 +268,30 @@ def profile_page():
 
 
 # movies list
-@app.route("/movieslist")
-def movie_list_page():
-    return render_template("movies-list.html", movies=movies)
+# @app.route("/movieslist")
+# def movie_list_page():
+#     return render_template("movies-list.html", movies=movies)
 
 
 # specific movie page
-@app.route("/movieslist/<id>")
-def movie_page(id):
-    # movie = get_specific_movie(id)
-    specific_movie = next((movie for movie in movies if movie["id"] == id), None)
+# @app.route("/movieslist/<id>")
+# def movie_page(id):
+# movie = get_specific_movie(id)
+# specific_movie = next((movie for movie in movies if movie["id"] == id), None)
 
-    if specific_movie is None:
-        result = {"message": "movie not found"}
-        return "<h1>Movie not found</h1>"
+# if specific_movie is None:
+#     result = {"message": "movie not found"}
+#     return "<h1>Movie not found</h1>"
 
-    # result = {"message": "movie successfully found", "data": specific_movie}
-    return render_template("movie.html", movie=specific_movie)
+# result = {"message": "movie successfully found", "data": specific_movie}
+# return render_template("movie.html", movie=specific_movie)
 
 
 @app.route("/login", methods=["GET"])  # HOF
 def login_page():
+    # How to get GET request data
+    username = request.args.get("username")
+    password = request.args.get("password")
     return render_template("forms.html")
 
 
@@ -215,10 +341,10 @@ def new_movie_list():
 
 
 # GET -> movies page -> JSON
-@app.get("/movies")
-def get_movies():
-    # have to convert to JSON (using jsonify library from Flask)
-    return jsonify(movies)
+# @app.get("/movies")
+# def get_movies():
+# have to convert to JSON (using jsonify library from Flask)
+# return jsonify(movies)
 
 
 # POST -> request.json -> create movie + add to movies list -> JSON
@@ -248,23 +374,23 @@ def create_movie():
 
 
 # <variable_name> -> becomes the keyword argument to "get_specific_movie"
-@app.get("/movies/<id>")
-def get_specific_movie(id):
-    print(type(id))  # string
-    # specific_movie = [movie for movie in movies if int(movie["id"]) == int(id)]
+# @app.get("/movies/<id>")
+# def get_specific_movie(id):
+#     print(type(id))  # string
+# specific_movie = [movie for movie in movies if int(movie["id"]) == int(id)]
 
-    # or - generator expression + have to account for when nothing is found (default val = None)
-    specific_movie = next((movie for movie in movies if movie["id"] == id), None)
-    # print(type(specific_movie))
+# or - generator expression + have to account for when nothing is found (default val = None)
+# specific_movie = next((movie for movie in movies if movie["id"] == id), None)
+# print(type(specific_movie))
 
-    # if specific_movie == []:
-    if specific_movie is None:
-        result = {"message": "movie not found"}
-        return jsonify(result), 404
+# if specific_movie == []:
+# if specific_movie is None:
+#     result = {"message": "movie not found"}
+#     return jsonify(result), 404
 
-    # result = {"message": "movie successfully found", "data": specific_movie[0]}
-    result = {"message": "movie successfully found", "data": specific_movie}
-    return jsonify(result)
+# result = {"message": "movie successfully found", "data": specific_movie[0]}
+# result = {"message": "movie successfully found", "data": specific_movie}
+# return jsonify(result)
 
 
 # create delete API for movies
